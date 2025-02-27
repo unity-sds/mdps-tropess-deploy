4b6eb16a5935f66bc8ad22f08d087e23ea00bd87
James McDuffie <James.McDuffie@jpl.nasa.gov>
2025-02-27 11:04:03 -0800

    Add script for performing actions necessary to build and deploy our repos

---
+++ build_applications.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import shutil
+import logging
+import subprocess
+from argparse import ArgumentParser
+
+import boto3
+import yaml
+
+from unity_app_generator import interface as build_interface
+
+# Deploy artfacts back to this repo
+DEPLOY_BASE_DIR = os.path.realpath(os.path.dirname(__file__))
+APP_STATE_DIR = os.path.join(DEPLOY_BASE_DIR, ".app_state")
+
+DOCKER_IMAGE_NAMESPACE = "tropess"
+DEFAULT_DOCKER_IMAGE_TAG = "latest"
+
+# Describes which repositories to build and their default locations
+# These can get overriden on the command line
+SOURCE_REPOS = {
+    "muses_ingest": "git@github.jpl.nasa.gov:MUSES-Processing/mdps-muses-data-ingest.git",
+    "py_tropess":"git@github.jpl.nasa.gov:MUSES-Processing/py-tropess.git", 
+}
+
+# Directories under DEPLOY_BASE_DIR where to put artifact files
+ARTIFACT_DIRS = {
+    "muses_ingest": "mdps-muses-data-ingest",
+    "py_tropess": "py-tropess",
+}
+
+SOURCE_CWL_ARTIFACT_FILENAME = "process.cwl"
+DEST_CWL_ARTIFACT_FILENAME = "process-{project_name}-{venue_name}.cwl"
+EXAMPLE_JOB_INPUT_FILENAME = "example_job_input.json"
+
+logger = logging.getLogger()
+
+class DeployApp(object):
+
+    def __init__(self, app_name, project_name, venue_name):
+
+        self._verify_project_venue_name(project_name, venue_name)
+
+        self.app_name = app_name
+        self.project_name = project_name
+        self.venue_name = venue_name
+
+    def _verify_project_venue_name(self, project_name, venue_name):
+        "Load MDPS venue name from AWS parameter store"
+
+        client = boto3.client('ssm')
+        response = client.get_parameter(Name=f"/unity/{project_name}/{venue_name}/venue-name")
+
+        assert(response['Parameter']['Value'] == venue_name)
+
+    @property
+    def app_state_dir(self):
+        "Where artifacts for unity-app-generator are stored"
+        
+        # Keep a seperate state dir for each application
+        state_dir = os.path.join(APP_STATE_DIR, self.app_name)
+        if not os.path.exists(state_dir):
+            os.makedirs(state_dir)
+
+        return state_dir
+
+    def build_app(self, source_repo, image_tag=None):
+        "Build Docker image to be used for venue deployment"
+
+        logger.info(f"Building {self.app_name} from {source_repo} for {self.project_name}/{self.venue_name}")
+
+        # Clone repo into state dir
+        checkout_dir = os.path.join(self.app_state_dir, "repo")
+
+        build_interface.init(self.app_state_dir, source_repo, checkout_dir)
+
+        # Build a local Docker image
+        build_interface.build_docker(self.app_state_dir,
+                                     image_namespace=DOCKER_IMAGE_NAMESPACE,
+                                     image_tag=image_tag)
+
+    def deploy_for_venue(self):
+
+        logger.info(f"Pushing Docker image for {self.app_name} into {self.project_name}/{self.venue_name}")
+
+        # Push the image into ECR to set the remote URL
+        build_interface.push_ecr(self.app_state_dir)
+
+    def update_artifacts(self):
+        
+        logger.info(f"Capturing {self.app_name} artifacts for {self.project_name}/{self.venue_name}")
+        
+        # Create CWL file
+        build_interface.build_cwl(self.app_state_dir)
+
+        # Copy the CWL file into a location within the repo matching the project/venue names
+        app_artifact_dirname = ARTIFACT_DIRS[self.app_name]
+        source_cwl_fn = os.path.join(self.app_state_dir, "cwl", SOURCE_CWL_ARTIFACT_FILENAME)
+
+        dest_filename = DEST_CWL_ARTIFACT_FILENAME.format(project_name=self.project_name, venue_name=self.venue_name)
+        dest_cwl_fn = os.path.join(DEPLOY_BASE_DIR, app_artifact_dirname, dest_filename)
+
+        shutil.copyfile(source_cwl_fn, dest_cwl_fn)
+
+        # Recreate example_job_input.json
+        # Use make-template from cwl tool on the CWL we created
+        # Read in that YAML and merge with existing JSON
+        example_job_filename = os.path.join(app_artifact_dirname, EXAMPLE_JOB_INPUT_FILENAME)
+        with open(example_job_filename, "r") as ex_file:
+            ex_inp_contents = json.load(ex_file)
+
+        yaml_output = subprocess.check_output(f"cwltool --make-template {dest_cwl_fn}", shell=True)
+        yaml_contents = yaml.safe_load(yaml_output)
+
+        # Update template with existing file so any manually modified values are preserved
+        yaml_contents.update(ex_inp_contents)
+
+        with open(example_job_filename, "w") as ex_file:
+            json.dump(ex_inp_contents, ex_file, indent=2)
+
+def main():
+
+    parser = ArgumentParser(description="Build TROPESS apps for deployment in MDPS")
+
+    parser.add_argument("project_name", 
+        help="MDPS project name for the deployment")
+    
+    parser.add_argument("venue_name", 
+        help="MDPS venue name for the deployment")
+
+    for app_name, default_repo in SOURCE_REPOS.items():
+        parser.add_argument(f"--{app_name}", default=default_repo,
+                            help=f"Location of {app_name} source repository other than default: {default_repo}")
+
+    default_app_list = list(SOURCE_REPOS.keys())
+    parser.add_argument("--app", action="append", default=None,
+        help=f"Applications to build other than default of all: {default_app_list}")
+
+    parser.add_argument("-t", "--tag", dest="docker_tag", default=DEFAULT_DOCKER_IMAGE_TAG,
+        help=f"Docker tag to use for images instead of default: {DEFAULT_DOCKER_IMAGE_TAG}")
+    
+    parser.add_argument("--skip-build", action="store_true",
+        help="Skip Dockerfile build if has been built with previous execution of this program")
+
+    parser.add_argument("--verbose", "-v", action="store_true", default=False,
+        help=f"Enable verbose logging")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.app is not None:
+        app_list = args.app
+    else:
+        app_list = default_app_list
+
+    for app_name in app_list:
+        app_deploy = DeployApp(app_name, args.project_name, args.venue_name)
+        if not args.skip_build:
+            app_deploy.build_app(getattr(args, app_name), args.docker_tag)
+        app_deploy.deploy_for_venue()
+        app_deploy.update_artifacts()
+
+if __name__ == '__main__':
+    main()
